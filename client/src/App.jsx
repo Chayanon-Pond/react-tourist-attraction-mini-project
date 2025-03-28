dd4411148c437b4f0683661ef3b8d61a634e9755
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-03-28 10:13:08 +0700

    feat: setup TailwindCSS in project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      {/* Start coding here */}
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
